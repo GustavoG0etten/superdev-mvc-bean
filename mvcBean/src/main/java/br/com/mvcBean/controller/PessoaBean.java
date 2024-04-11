27c589f07a3a98e076b4aca54b5b5cdbfc44a9d0
GustavoG0etten <gustavogoetten87@gmail.com>
2024-04-10 22:05:01 -0300

    :construction: feat(pessoa): Gerar sequencia ID

--- mvcBean/src/main/java/br/com/mvcBean/controller/PessoaBean.java
+++ mvcBean/src/main/java/br/com/mvcBean/controller/PessoaBean.java
@@ -21,11 +21,31 @@ public class PessoaBean implements Serializable {
 	
 	List<Pessoa> pessoas = new ArrayList<Pessoa>();
 	
+	// CRIAR UM METODO QUE SEJA CAPAZ DE INSERIR O ID AO USUARIO
+	// DE FORMA SEQUENCIAL, OU SEJA, A CLASSE BEAN DEVE CONTROLAR 
+	// A SEQUENCIA DO ID DAS PESSOAS
+	
+	
 	public String adicionar() {
+		pessoa.setId(adicionarId());
 		pessoas.add(pessoa);
 		pessoa = new Pessoa();
 		System.out.println(pessoas.size());
 		return null;
+	}
+	
+	private int adicionarId() {
+		if (pessoas.isEmpty()) {
+			return 0;
+		} else {
+			int maiorId = 0;
+	        for (Pessoa p : pessoas) {
+	            if (p.getId() > maiorId) {
+	                maiorId = p.getId();
+	            }
+	        }
+	        return maiorId + 1; 
+		}
 	}
 
 	public Pessoa getPessoa() {
